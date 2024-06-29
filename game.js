6209b1a566deeae8749ad40a256056b1f3e72b45
shazianaz47 <167789437+shazianaz47@users.noreply.github.com>
2024-06-29 16:05:57 +0500

    created simple cli based number guessing game

---
+++ game.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+//1) computer will genrate random number
+//2) user input for guessing number
+//3) compare user input with computer genrated to show result
+let guess = Math.floor(Math.random() * 6 + 1);
+let guessNumber = await inquirer.prompt({
+    message: "Enter Your Number Between 1-6:",
+    type: "number",
+    name: "Yourguess",
+});
+if (guessNumber.Yourguess === guess) {
+    console.log("Congratulation!!!! Your Guess is Right....................");
+}
+else {
+    console.log("!!!oops!!! Your Guess is Wrong.");
+}
